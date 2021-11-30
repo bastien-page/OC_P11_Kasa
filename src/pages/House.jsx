d68cdd70329f68f2afbee909be610e51ec6f9da2
Bastien <baba2sgt@gmail.com>
2021-11-30 15:50:58 +0100

    Error

--- src/pages/House.jsx
+++ src/pages/House.jsx
@@ -6,15 +6,23 @@ import Collapse from "../components/Collapse";
 import Footer from "../components/Footer";
 
 import data from "../logements.json";
+import NotFound from "./NotFound";
 
 export class House extends Component {
   render() {
     const dataHouse = data;
+
     const id = window.location.pathname.substr(7);
 
     const home = dataHouse.find((elt) => elt.id === id);
 
-    return (
+    /*
+    Si on ne trouve pas l'id dans les données on affiche l'Error 
+    */
+
+    return home === undefined ? (
+      <NotFound />
+    ) : (
       <>
         <Header />
         <Carrousel pictures={home.pictures} />
